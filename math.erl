e5fa4797b869bb82901bc0e1bf1b5e1486443166
Abourass <abourassa@assetval.com>
2020-02-21 13:01:12 -0700

    :sparkles: Added factorial / multiply Fn

--- math.erl
+++ math.erl
@@ -1,15 +1,17 @@
 %%%-------------------------------------------------------------------
 %%% @author navi
-%%% @copyright (C) 2020, <COMPANY>
+%%% @copyright (C) 2020, <Anandamide.IO>
 %%% @doc
 %%%
 %%% @end
 %%% Created : 21. Feb 2020 12:45 PM
 %%%-------------------------------------------------------------------
--module(tut).
+-module(math).
 -author("navi").
 
 %% API
--export([double/1]).
+-export([double/1, factorial/1, multiply/2]).
 
 double(X) -> 2 * X.
+factorial(1) -> 1; factorial(N) -> N * factorial(N - 1).
+multiply(X, Y) -> X * Y.
